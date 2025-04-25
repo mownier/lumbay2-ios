48ad45a3786a5f596928dd48648e394939534559
Mounir Ybanez <rinuom91@gmail.com>
2025-04-25 22:44:18 +0800

    Initial commit.

---
+++ Lumbay2/Lumbay2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lumbay2App.swift
+//  Lumbay2
+//
+//  Created by nir on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Lumbay2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
